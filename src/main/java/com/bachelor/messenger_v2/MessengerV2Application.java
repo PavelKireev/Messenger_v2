7e32558912fe75bda6f92a440a6551eb209ff61f
pavelkireev <pkireev97@gmail.com>
2022-06-13 16:33:37 +0200

    Initial commit

---
+++ src/main/java/com/bachelor/messenger_v2/MessengerV2Application.java
@@ -0,0 +1,13 @@
+package com.bachelor.messenger_v2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MessengerV2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MessengerV2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/bachelor/messenger_v2/MessengerV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bachelor.messenger_v2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MessengerV2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
